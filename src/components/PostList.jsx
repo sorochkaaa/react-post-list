6b5d236468108c0fe23bded4d0db57c4e2af8fb9
sorochkaaa <akovalenko199910@yandex.ru>
2023-04-02 22:25:14 +0700

    Added simple animation

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import PostItem from "./PostItem";
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 function PostList({posts, ...props}) {
     return (
         <div>
-            {posts.length 
-                ? posts.map((post, index) => <PostItem {...props} index={index + 1} key={post.id} post={post}></PostItem>)
-                : <h2 style={{textAlign: "center"}}>Посты не найдены!!!</h2>
-            }
+            <TransitionGroup>
+                {posts.length 
+                    ? posts.map((post, index) => {
+                        return (
+                            <CSSTransition
+                                key={post.id}
+                                timeout={500}
+                                classNames="post"
+                            >
+                                <PostItem {...props} index={index + 1} post={post}></PostItem>
+                            </CSSTransition>
+                        )
+                    })
+                    : <h2 style={{textAlign: "center"}}>Посты не найдены!!!</h2>
+                }
+            </TransitionGroup>
         </div>
     )
 }
